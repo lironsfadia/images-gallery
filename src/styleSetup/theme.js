3238c74eb4398fb89b61d88cf38a6790ac5af1a5
Liron <liron@trellis.ai>
2022-04-23 00:24:55 +0300

    Added metrial ui theme for the typography items

---
+++ src/styleSetup/theme.js
@@ -0,0 +1,35 @@
+import { createTheme } from '@mui/material/styles'
+
+export const theme = createTheme();
+theme.typography.h3 = {
+  fontSize: '1.2rem',
+  fontFamily: ['"Segoe UI"','Roboto','Ubuntu','sans-serif'],
+  '@media (min-width:600px)': {
+    fontSize: '1.5rem',
+  },
+  [theme.breakpoints.up('md')]: {
+    fontSize: '2.4rem',
+  },
+};
+
+theme.typography.h5 = {
+  fontSize: '0.7rem',
+  fontFamily: ['"Open Sans"', 'sans-serif'],
+  '@media (min-width:600px)': {
+    fontSize: '1.4rem',
+  },
+  [theme.breakpoints.up('md')]: {
+    fontSize: '2.4rem',
+  },
+};
+
+theme.typography.body2 = {
+  fontSize: '0.6rem',
+  fontFamily: ['"Open Sans"', 'sans-serif'],
+  '@media (min-width:600px)': {
+    fontSize: '1.0rem',
+  },
+  [theme.breakpoints.up('md')]: {
+    fontSize: '1.3rem',
+  },
+};
